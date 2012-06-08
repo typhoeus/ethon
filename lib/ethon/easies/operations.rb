6f6306af33c68838df594aaf5a50b995ffdbaab8
Hans Hasselberg <me@hans.io>
2012-06-08 17:58:35 +0200

    Document Easy::Operations.

--- lib/ethon/easies/operations.rb
+++ lib/ethon/easies/operations.rb
@@ -1,10 +1,24 @@
 module Ethon
   module Easies
+    # This module contains the logic to prepare and perform
+    # an easy.
     module Operations
+
+      # Perform the easy request.
+      #
+      # @example Perform the request.
+      #   easy.perform
+      #
+      # @return [ Integer ] The return code.
       def perform
         @return_code = Curl.easy_perform(handle)
       end
 
+      # Prepare the easy. Options, headers and callbacks
+      # were set.
+      #
+      # @example Prepare easy.
+      #   easy.prepare
       def prepare
         set_options
         set_headers
