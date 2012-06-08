f7f7970347ac31a0a27e471931b741f1c3a70c4d
Hans Hasselberg <me@hans.io>
2012-06-08 17:16:20 +0200

    Document Easy.

--- lib/ethon/easy.rb
+++ lib/ethon/easy.rb
@@ -9,6 +9,9 @@ require 'ethon/easies/http'
 require 'ethon/easies/operations'
 
 module Ethon
+
+  # This is the class representing the libcurl easy interface
+  # See http://curl.haxx.se/libcurl/c/libcurl-easy.html for more informations.
   class Easy
     include Ethon::Easies::Informations
     include Ethon::Easies::Callbacks
@@ -29,6 +32,15 @@ module Ethon
       end
     end
 
+    # Initialize a new Easy.
+    # It initializes curl, if not already done and applies the provided options.
+    #
+    # @example Create a new Easy.
+    #   Easy.new(:url => "www.google.de")
+    #
+    # @param [ Hash ] options The options to set.
+    #
+    # @return [ Easy ] A new Easy.
     def initialize(options = {})
       Curl.init
       ObjectSpace.define_finalizer(self, self.class.finalizer(self))
@@ -38,6 +50,12 @@ module Ethon
       end
     end
 
+    # Returns a  pointer to the curl easy handle.
+    #
+    # @example Return the handle.
+    #   easy.handle
+    #
+    # @return [ FFI::Pointer ] A pointer to the curl easy handle.
     def handle
       @handle ||= Curl.easy_init
     end
