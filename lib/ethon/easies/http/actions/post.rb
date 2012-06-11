e90b0c94ee43eea5743ba0c52c3fa6c717984549
Hans Hasselberg <me@hans.io>
2012-06-11 15:32:37 +0200

    Document Post and Put.

--- lib/ethon/easies/http/actions/post.rb
+++ lib/ethon/easies/http/actions/post.rb
@@ -2,19 +2,41 @@ module Ethon
   module Easies
     module Http
       module Actions
+
+        # This class knows everything about making POST requests.
         class Post < Action
+
+          # Setup everything what is necessary for a proper
+          # POST request.
+          #
+          # @example setup.
+          #   post.setup(easy)
+          #
+          # @param [ easy ] easy the easy to setup.
           def setup(easy)
             set_nothing(easy) if params.empty? && form.empty?
             set_params(easy) unless params.empty?
             set_form(easy) unless form.empty?
           end
 
+          # Set things up when neither params nor body is provided.
+          #
+          # @example Setup.
+          #   post.set_nothing(easy)
+          #
+          # @parms [ Easy ] easy The easy to setup.
           def set_nothing(easy)
             easy.url = url
             easy.postfieldsize = 0
             easy.copypostfields = ""
           end
 
+          # Set things up when params is provided.
+          #
+          # @example Setup.
+          #   post.set_params(easy)
+          #
+          # @parms [ Easy ] easy The easy to setup.
           def set_params(easy)
             params.escape = true
             easy.url = "#{url}?#{params.to_s}"
@@ -22,6 +44,13 @@ module Ethon
             easy.copypostfields = ""
           end
 
+          # Set things up when form is provided.
+          # Deals with multipart forms.
+          #
+          # @example Setup.
+          #   post.set_form(easy)
+          #
+          # @parms [ Easy ] easy The easy to setup.
           def set_form(easy)
             easy.url ||= url
             if form.multipart?
--- lib/ethon/easies/http/actions/put.rb
+++ lib/ethon/easies/http/actions/put.rb
@@ -2,19 +2,41 @@ module Ethon
   module Easies
     module Http
       module Actions
+
+        # This class knows everything about making PUT requests.
         class Put < Action
+
+          # Setup everything what is necessary for a proper
+          # PUT request.
+          #
+          # @example setup.
+          #   put.setup(easy)
+          #
+          # @param [ easy ] easy the easy to setup.
           def setup(easy)
             set_nothing(easy) if params.empty? && form.empty?
             set_params(easy) unless params.empty?
             set_upload(easy) unless form.empty?
           end
 
+          # Set things up when neither params nor body is provided.
+          #
+          # @example Setup.
+          #   put.set_nothing(easy)
+          #
+          # @parms [ Easy ] easy The easy to setup.
           def set_nothing(easy)
             easy.url = url
             easy.upload = true
             easy.infilesize = 0
           end
 
+          # Set things up when params is provided.
+          #
+          # @example Setup.
+          #   put.set_params(easy)
+          #
+          # @parms [ Easy ] easy The easy to setup.
           def set_params(easy)
             params.escape = true
             easy.url = "#{url}?#{params.to_s}"
@@ -22,6 +44,13 @@ module Ethon
             easy.infilesize = 0
           end
 
+          # Set things up when form is provided.
+          # Deals with multipart forms.
+          #
+          # @example Setup.
+          #   put.set_form(easy)
+          #
+          # @parms [ Easy ] easy The easy to setup.
           def set_upload(easy)
             easy.url ||= url
             easy.upload = true
