fb588d5eaa42b620b50787ea9dea37850d194d9f
Hans Hasselberg <me@hans.io>
2012-06-11 15:17:36 +0200

    Document Action.

--- lib/ethon/easies/http/action.rb
+++ lib/ethon/easies/http/action.rb
@@ -1,14 +1,31 @@
 module Ethon
   module Easies
     module Http
+      # This class represents a Http Action and is a factory
+      # for more real actions like GET, HEAD, POST and PUT.
       class Action
         attr_reader :url, :params, :form
 
         class << self
+
+          # Return the corresponding action class.
+          #
+          # @example Return the action.
+          #   Action.fabricate(:get)
+          #
+          # @param [ String ] action_name The action name.
+          #
+          # @return [ Class ] The action class.
           def fabricate(action_name)
             eval("Actions::#{action_name.capitalize}")
           end
 
+          # Resets every attribute possibly set by an action.
+          #
+          # @example Reset attributes,
+          #   Action.reset(easy)
+          #
+          # @param [ Easy ] easy The easy to reset.
           def reset(easy)
             easy.url = nil
             easy.httpget = nil
@@ -22,6 +39,15 @@ module Ethon
           end
         end
 
+        # Create a new action.
+        #
+        # @example Create a new action.
+        #   Action.new("www.example.com", {})
+        #
+        # @param [ String ] url The url.
+        # @param [ Hash ] options The options.
+        #
+        # @return [ Action ] A new action.
         def initialize(url, options)
           @url = url
           @params = Params.new(options[:params])
