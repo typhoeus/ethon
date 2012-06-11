cf829663066602d5a92ea41dda3a4132b46ece56
Hans Hasselberg <me@hans.io>
2012-06-11 15:28:58 +0200

    Document get.

--- lib/ethon/easies/http/actions/get.rb
+++ lib/ethon/easies/http/actions/get.rb
@@ -2,7 +2,17 @@ module Ethon
   module Easies
     module Http
       module Actions
+
+        # This class knows everything about making GET requests.
         class Get < Action
+
+          # Setup everything what is necessary for a proper
+          # GET request.
+          #
+          # @example Setup.
+          #   get.setup(easy)
+          #
+          # @param [ Easy ] easy The easy to setup.
           def setup(easy)
             easy.httpget = true
             if params.empty?
