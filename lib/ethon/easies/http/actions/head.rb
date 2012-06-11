658dc40cad61d0529a637160cd1e3e84035d6f1f
Hans Hasselberg <me@hans.io>
2012-06-11 15:34:03 +0200

    Document head.

--- lib/ethon/easies/http/actions/head.rb
+++ lib/ethon/easies/http/actions/head.rb
@@ -2,7 +2,17 @@ module Ethon
   module Easies
     module Http
       module Actions
+
+        # This class knows everything about making HEAD requests.
         class Head < Action
+
+          # Setup everything what is necessary for a proper
+          # HEAD request.
+          #
+          # @example Setup.
+          #   head.setup(easy)
+          #
+          # @param [ Easy ] easy The easy to setup.
           def setup(easy)
             easy.nobody = true
             if params.empty?
