2b03a861044efbfd8de2f9e41b4e5807d34a8671
Hans Hasselberg <me@hans.io>
2012-06-11 15:11:42 +0200

    Document http.

--- lib/ethon/easies/http.rb
+++ lib/ethon/easies/http.rb
@@ -6,7 +6,18 @@ require 'ethon/easies/http/actions/put'
 
 module Ethon
   module Easies
+
+    # This module contains logic about making valid http requests.
     module Http
+
+      # Set specified options in order to make a http request.
+      #
+      # @example Set options for http request.
+      #   easy.http_request("www.google.com", :get, {})
+      #
+      # @param [ String ] url The url.
+      # @param [ String ] action_name The http action name.
+      # @param [ Hash ] options The options hash.
       def http_request(url, action_name, options = {})
         Action.reset(self)
         Action.fabricate(action_name).new(url, options).setup(self)
