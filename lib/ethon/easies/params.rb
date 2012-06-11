4e31e66850b1c3205b8916bc3fa2077195931617
Hans Hasselberg <me@hans.io>
2012-06-11 15:40:48 +0200

    Document params.

--- lib/ethon/easies/params.rb
+++ lib/ethon/easies/params.rb
@@ -2,22 +2,50 @@ require 'ethon/easies/util'
 
 module Ethon
   module Easies
+
+    # This class represents http request parameters.
     class Params
       include Ethon::Easies::Util
       attr_accessor :escape
 
+      # Create a new Params.
+      #
+      # @example Create a new Params.
+      #   Params.new({})
+      #
+      # @params [ Hash ] params The params to use.
+      #
+      # @return [ Params ] A new Params.
       def initialize(params)
         @params = params || {}
       end
 
+      # Return the string representation of params.
+      #
+      # @example Return string representation.
+      #   params.to_s
+      #
+      # @return [ String ] The string representation.
       def to_s
         query_pairs.map{|pair| pair.join("=")}.join('&')
       end
 
+      # Return the query pairs.
+      #
+      # @example Return the query pairs.
+      #   params.query_pairs
+      #
+      # @return [ Array ] The query pairs.
       def query_pairs
         @query_pairs ||= build_query_pairs_from_hash(@params, escape)
       end
 
+      # Return wether there are elements in the params or not.
+      #
+      # @example Return if params is empty.
+      #   params.empty?
+      #
+      # @return [ Boolean ] True if params is empty, else false.
       def empty?
         @params.empty?
       end
