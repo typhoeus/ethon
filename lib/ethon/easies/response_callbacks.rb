6b3aa0d4135b047bf26438f2bcfb8bbfe4160439
Hans Hasselberg <me@hans.io>
2012-06-19 00:05:41 +0200

    Remove on_success and on_failure callbacks.
    The on_complete callback should be enough.

--- lib/ethon/easies/response_callbacks.rb
+++ lib/ethon/easies/response_callbacks.rb
@@ -1,69 +1,9 @@
 module Ethon
   module Easies
+
+    # This module contains the logic for the response callbacks.
+    # The on_complete callback is the only one at the moment.
     module ResponseCallbacks
-      def trigger_callbacks
-        complete
-        success if success?
-        failure if failure?
-      end
-
-      # Return wether the request was successful.
-      # Curls return code needs to be 0 and the response
-      # code between 200 and 299.
-      #
-      # @example Was request successful?
-      #   easy.success?
-      #
-      # @return [ Boolean ] True if request successful, false else.
-      def success?
-        return_code == 0 && [0, *(200..299)].include?(response_code)
-      end
-
-      # Return wether the request failed.
-      # Returns the opposite of successful?.
-      #
-      # @example Did request failes?
-      #   easy.failure?
-      #
-      # @return [ Boolean ] True if request failed, false else.
-      def failure?
-        !success?
-      end
-
-      # Execute preset success callback.
-      #
-      # @example Execute success callback.
-      #   easy.success
-      def success
-        return if !defined?(@success) || @success.nil?
-        @success.call(self)
-      end
-
-      # Set success callback.
-      #
-      # @example Set success callback.
-      #   easy.on_success = block
-      def on_success(&block)
-        @success = block
-      end
-
-      # Execute preset failure callback.
-      #
-      # @example Execute failure callback.
-      #   easy.failure
-      def failure
-        return if !defined?(@failure) || @failure.nil?
-        @failure.call(self)
-      end
-
-      # Set failure callback.
-      #
-      # @example Set failure callback.
-      #   easy.on_failure = block
-      def on_failure(&block)
-        @failure = block
-      end
-
       # Execute preset complete callback.
       #
       # @example Execute complete callback.
--- spec/ethon/easies/response_callbacks_spec.rb
+++ spec/ethon/easies/response_callbacks_spec.rb
@@ -3,74 +3,21 @@ require 'spec_helper'
 describe Ethon::Easies::ResponseCallbacks do
   let(:easy) { Ethon::Easy.new }
 
-  describe "#trigger_callbacks" do
-    it
+  describe "#on_complete=" do
+    it "assigns value" do
+      easy.on_complete {}
+      easy.instance_variable_get("@complete").should be_a(Proc)
+    end
   end
 
-  describe "#success?" do
+  describe "#complete" do
     before do
-      easy.return_code = return_code
-      Ethon::Curl.
-        expects(:get_info_long).
-        with(:response_code, easy.handle).
-        returns(response_code).at_most_once
+      easy.on_complete {|e| e.response_code }
+      Ethon::Curl.expects(:get_info_long)
     end
 
-    context "when return code not 0" do
-      let(:return_code) { 2 }
-      let(:response_code) { 0 }
-
-      it "returns false" do
-        easy.success?.should be_false
-      end
-    end
-
-    context "when return code 0" do
-      let(:return_code) { 0 }
-
-      context "when response code 0" do
-        let(:response_code) { 0 }
-
-        it "returns true" do
-          easy.success?.should be_true
-        end
-      end
-
-      context "when response code between 200 and 299" do
-        let(:response_code) { 201 }
-
-        it "returns true" do
-          easy.success?.should be_true
-        end
-      end
-
-      context "when response code not 0 and not between 200 and 299" do
-        let(:response_code) { 301 }
-
-        it "returns false" do
-          easy.success?.should be_false
-        end
-      end
-    end
-  end
-
-  [:success, :failure, :complete].each do |name|
-    describe "#on_#{name}=" do
-      it "assigns value" do
-        eval "easy.on_#{name} {}"
-        easy.instance_variable_get("@#{name}").should be_a(Proc)
-      end
-    end
-
-    describe "##{name}" do
-      before do
-        eval "easy.on_#{name} {|e| e.response_code }"
-        Ethon::Curl.expects(:get_info_long)
-      end
-
-      it "executes block and passes self" do
-        easy.method(name).call
-      end
+    it "executes block and passes self" do
+      easy.complete
     end
   end
 end
