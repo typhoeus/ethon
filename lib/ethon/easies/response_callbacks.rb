f6ac1f5f5e3c922d12f2442016a50152f4ec7141
Hans Hasselberg <me@hans.io>
2012-06-28 17:47:30 +0200

    Easy#on_complete handles multiple callbacks.

--- lib/ethon/easies/response_callbacks.rb
+++ lib/ethon/easies/response_callbacks.rb
@@ -3,22 +3,45 @@ module Ethon
 
     # This module contains the logic for the response callbacks.
     # The on_complete callback is the only one at the moment.
+    #
+    # You can set multiple callbacks, which are then executed
+    # in the same order.
+    #
+    #   easy.on_complete { p 1 }
+    #   easy.on_complete { p 2 }
+    #   easy.complete
+    #   #=> 1
+    #   #=> 2
+    #
+    # You can clear the callbacks:
+    #
+    #   easy.on_complete { p 1 }
+    #   easy.on_complete { p 2 }
+    #   easy.on_complete.clear
+    #   easy.on_complete
+    #   #=> []
     module ResponseCallbacks
-      # Execute preset complete callback.
+
+      # Set on_complete callback.
       #
-      # @example Execute complete callback.
-      #   easy.complete
-      def complete
-        return if !defined?(@complete) || @complete.nil?
-        @complete.call(self)
+      # @example Set on_complete.
+      #   request.on_complete { p "yay" }
+      #
+      # @param [ Block ] block The block to execute.
+      def on_complete(&block)
+        @on_complete ||= []
+        @on_complete << block if block_given?
+        @on_complete
       end
 
-      # Set complete callback.
+      # Execute on_complete callbacks.
       #
-      # @example Set complete callback.
-      #   easy.on_complete = block
-      def on_complete(&block)
-        @complete = block
+      # @example Execute on_completes.
+      #   request.complete
+      def complete
+        if defined?(@on_complete)
+          @on_complete.map{ |callback| callback.call(self) }
+        end
       end
     end
   end
--- spec/ethon/easies/response_callbacks_spec.rb
+++ spec/ethon/easies/response_callbacks_spec.rb
@@ -3,20 +3,40 @@ require 'spec_helper'
 describe Ethon::Easies::ResponseCallbacks do
   let(:easy) { Ethon::Easy.new }
 
-  describe "#on_complete=" do
-    it "assigns value" do
-      easy.on_complete {}
-      easy.instance_variable_get("@complete").should be_a(Proc)
+  describe "#on_complete" do
+    it "responds" do
+      easy.should respond_to(:on_complete)
+    end
+
+    context "when no block given" do
+      it "returns @on_complete" do
+        easy.on_complete.should eq([])
+      end
+    end
+
+    context "when block given" do
+      it "stores" do
+        easy.on_complete { p 1 }
+        easy.instance_variable_get(:@on_complete).should have(1).items
+      end
+    end
+
+    context "when multiple blocks given" do
+      it "stores" do
+        easy.on_complete { p 1 }
+        easy.on_complete { p 2 }
+        easy.instance_variable_get(:@on_complete).should have(2).items
+      end
     end
   end
 
   describe "#complete" do
     before do
-      easy.on_complete {|e| e.response_code }
-      Ethon::Curl.expects(:get_info_long)
+      easy.on_complete {|r| String.new(r.url) }
     end
 
-    it "executes block and passes self" do
+    it "executes blocks and passes self" do
+      String.expects(:new).with(easy.url)
       easy.complete
     end
   end
