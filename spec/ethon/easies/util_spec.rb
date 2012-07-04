9488e395be86a4cf51d78aa9b4a438b2f8448d03
Hans Hasselberg <me@hans.io>
2012-07-04 09:32:02 +0200

    Add specs for Util.escape_zero_byte.

--- spec/ethon/easies/util_spec.rb
+++ spec/ethon/easies/util_spec.rb
@@ -1,10 +1,27 @@
 require 'spec_helper'
 
 describe Ethon::Easies::Util do
-  let(:hash) { {} }
-  let(:params) { Ethon::Easies::Params.new(hash) }
+  class Dummy
+    include Ethon::Easies::Util
+  end
+
+  let(:klass) { Dummy.new }
 
   describe "escape_zero_byte" do
-    it
+    context "when value has no zero byte" do
+      let(:value) { "hello world" }
+
+      it "returns same value" do
+        klass.escape_zero_byte(value).should be(value)
+      end
+    end
+
+    context "when value has zero byte" do
+      let(:value) { "hello \0world" }
+
+      it "returns escaped" do
+        klass.escape_zero_byte(value).should eq("hello \\0world")
+      end
+    end
   end
 end
