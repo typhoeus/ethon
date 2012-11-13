63df5aa710c3cfff2ac3c9902ae82d0e009b15c0
Hans Hasselberg <me@hans.io>
2012-11-13 09:22:11 +0100

    Do not try to attach function getdtablesize in windows.

--- lib/ethon/libc.rb
+++ lib/ethon/libc.rb
@@ -2,7 +2,15 @@ module Ethon
   module Libc
     extend FFI::Library
     ffi_lib 'c'
-    attach_function :getdtablesize, [], :int
-    attach_function :free, [:pointer], :void
+
+    # :nodoc:
+    def self.windows?
+      !(RbConfig::CONFIG['host_os'] !~ /mingw|mswin|bccwin/)
+    end
+
+    unless windows?
+      attach_function :getdtablesize, [], :int
+      attach_function :free, [:pointer], :void
+    end
   end
 end
