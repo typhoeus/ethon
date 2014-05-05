d8d9f00b7fbb29e011410bb955796d7aa3b1aefd
Tasos Laskos <tasos.laskos@gmail.com>
2014-05-05 14:39:44 +0300

    libc_spec: Use hash-rockets

--- spec/ethon/libc_spec.rb
+++ spec/ethon/libc_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
 describe Ethon::Libc do
-  describe "#getdtablesize", if: !Ethon::Curl.windows? do
+  describe "#getdtablesize", :if => !Ethon::Curl.windows? do
     it "returns an integer" do
       expect(Ethon::Libc.getdtablesize).to be_a(Integer)
     end
 
-    it "returns bigger zero", if: !Ethon::Curl.windows? do
+    it "returns bigger zero", :if => !Ethon::Curl.windows? do
       expect(Ethon::Libc.getdtablesize).to_not be_zero
     end
   end
