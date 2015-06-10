9d51a5a1a9a8113c1efb8e69150c6e52ba145d47
John Doe <author@example.com>
2015-06-11 00:05:02 +0300

    #headers: check @headers_called

--- lib/ethon/easy/callbacks.rb
+++ lib/ethon/easy/callbacks.rb
@@ -36,7 +36,7 @@ module Ethon
       # @return [ Proc ] The callback.
       def body_write_callback
         @body_write_callback ||= proc do |stream, size, num, object|
-          headers unless @headers_called
+          headers
           result = body(chunk = stream.read_string(size * num))
           @response_body << chunk if result == :unyielded
           result != :abort ? size * num : -1
