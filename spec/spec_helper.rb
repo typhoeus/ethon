bf554874f7d696f1b56394328b675e168243963b
Kevin Gilpin <kgilpin@gmail.com>
2022-06-01 13:27:35 -0400

    dev: Add conditional tracing of specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,9 +5,34 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 require 'bundler'
 Bundler.setup
 
+def trace?
+  ENV['TRACE'] == 'true'
+end
+
 require 'appmap/rspec'
 
+if trace?
+  stack = {}
+  $tp = TracePoint.trace(:call, :return) do |tp|
+    stack[Thread.current] ||= []
+    (0..stack[Thread.current].length).each { $stdout.write '  '}
+    $stdout.write [tp.path, tp.lineno, tp.event].join(' ')
+    $stdout.puts
+    if tp.event == :call
+      stack[Thread.current] << tp.event
+    else
+      stack[Thread.current].pop
+    end
+  end
+  $tp.enable
+end
+
 require "ethon"
+
+if $tp
+  $tp.disable
+end
+
 require 'rspec'
 
 if defined? require_relative
