692a20dab2c3110c8cdd782929c1d446ac7a8a46
Sergey Tarasov <dzirtusss@gmail.com>
2023-11-03 14:11:20 +0200

    Change to libcurl-impersonate-chrome

--- lib/ethon/curls/settings.rb
+++ lib/ethon/curls/settings.rb
@@ -7,6 +7,6 @@ module Ethon
     callback :debug_callback, [:pointer, :debug_info_type, :pointer, :size_t, :pointer], :int
     callback :progress_callback, [:pointer, :long_long, :long_long, :long_long, :long_long], :int
     ffi_lib_flags :now, :global
-    ffi_lib ['libcurl', 'libcurl.so.4']
+    ffi_lib ['libcurl-impersonate-chrome', 'libcurl-impersonate-chrome.so.4']
   end
 end
